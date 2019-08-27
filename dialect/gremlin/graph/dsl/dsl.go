bd07c86b60500fd174af51a00474bbf32da48a19
Ariel Mashraki <mashraki@fb.com>
2019-08-27 04:47:07 -0700

    all: add license header to all go files
    Summary:
    Used addlicense to generate this:
     addlicense -c "Facebook Inc" -f license_header .
    
    example was taken from: https://github.com/facebook/litho/blob/master/lib/soloader/BUCK
    
    Reviewed By: alexsn
    
    Differential Revision: D17070152
    
    fbshipit-source-id: e7b91398d7f6181727be3400c1872ad5f28e38ed

--- dialect/gremlin/graph/dsl/dsl.go
+++ dialect/gremlin/graph/dsl/dsl.go
@@ -1,3 +1,7 @@
+// Copyright 2019-present Facebook Inc. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
 // Package dsl provide an API for writing gremlin dsl queries almost as-is
 // in Go without using strings in the code.
 //
--- entc/integration/generate.go
+++ entc/integration/generate.go
@@ -1,3 +1,7 @@
+// Copyright 2019-present Facebook Inc. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
 package integration
 
 //go:generate go run ../cmd/entc/entc.go generate --storage=sql,gremlin --idtype string --header "Code generated (@generated) by entc, DO NOT EDIT." ./ent/schema
