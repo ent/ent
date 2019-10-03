86c14d7a3cdd9fda341820fe5b13accdf7e0893c
Ariel Mashraki <mashraki@fb.com>
2019-10-02 22:58:46 -0700

    ent/examples: add readme to examples and ga trackid
    Reviewed By: alexsn
    
    Differential Revision: D17733452
    
    fbshipit-source-id: 1d7d472f7fe38989246f434cf7864c0e546126c1

--- doc/website/siteConfig.js
+++ doc/website/siteConfig.js
@@ -107,6 +107,7 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+  gaTrackingId: 'UA-44373548-44'
 };
 
 module.exports = siteConfig;
