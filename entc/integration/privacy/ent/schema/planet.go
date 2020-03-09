4ca247debf303b152d9b62829b41c3c40358ad10
Alex Snast <alexsn@fb.com>
2020-03-09 14:02:56 +0200

    entc/privacy: adding privacy template

---
+++ entc/integration/privacy/ent/schema/planet.go
@@ -0,0 +1,48 @@
+package schema
+
+import (
+	"context"
+	"log"
+
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/entc/integration/privacy/ent/privacy"
+	"github.com/facebookincubator/ent/schema/edge"
+	"github.com/facebookincubator/ent/schema/field"
+)
+
+// Planet defines the schema of a planet.
+type Planet struct {
+	ent.Schema
+}
+
+func (Planet) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name").
+			Immutable().
+			NotEmpty().
+			Unique(),
+		field.Uint("age").
+			Optional(),
+	}
+}
+
+func (Planet) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("neighbors", Planet.Type),
+	}
+}
+
+func (Planet) Hooks() []ent.Hook {
+	return []ent.Hook{
+		func(next ent.Mutator) ent.Mutator {
+			return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
+				log.Println("Planet mutation of type", m.Type())
+				return next.Mutate(ctx, m)
+			})
+		},
+	}
+}
+
+func (Planet) Policy() ent.Policy {
+	return privacy.AlwaysAllowReadWrite()
+}
