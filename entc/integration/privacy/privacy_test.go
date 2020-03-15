84d74ad8bd1db7092683680a391cc2eb9cab8531
Ariel Mashraki <ariel@mashraki.co.il>
2020-03-15 23:53:22 +0200

    update circleci go orb

--- entc/integration/privacy/privacy_test.go
+++ entc/integration/privacy/privacy_test.go
@@ -17,6 +17,7 @@ import (
 func TestPrivacyRules(t *testing.T) {
 	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
 	require.NoError(t, err)
+	defer client.Close()
 	ctx := context.Background()
 	err = client.Schema.Create(ctx)
 	require.NoError(t, err)
