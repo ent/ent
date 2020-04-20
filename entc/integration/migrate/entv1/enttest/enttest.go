ebf26b929006f35a59269d31f35eaf854ec619b3
Alex Snast <alexsn@fb.com>
2020-04-20 16:34:34 +0300

    entc/gen: finish enttest package tempalte
    Signed-off-by: Alex Snast <alexsn@fb.com>

--- entc/integration/migrate/entv1/enttest/enttest.go
+++ entc/integration/migrate/entv1/enttest/enttest.go
@@ -9,27 +9,72 @@ package enttest
 import (
 	"context"
 
-	"github.com/facebookincubator/ent/entc/integration/migrate/entv1"
-	_ "github.com/facebookincubator/ent/entc/integration/migrate/entv1/runtime"
-
 	"github.com/facebookincubator/ent/dialect/sql/schema"
+	"github.com/facebookincubator/ent/entc/integration/migrate/entv1"
+
+	// required by schema hooks.
+	_ "github.com/facebookincubator/ent/entc/integration/migrate/entv1/runtime"
 )
 
-// TestingT is the interface that is shared between
-// testing.T and testing.B and used by enttest.
-type TestingT interface {
-	FailNow()
-	Error(...interface{})
+type (
+	// TestingT is the interface that is shared between
+	// testing.T and testing.B and used by enttest.
+	TestingT interface {
+		FailNow()
+		Error(...interface{})
+	}
+
+	// Option configures client creation.
+	Option func(*options)
+
+	options struct {
+		opts        []entv1.Option
+		migrateOpts []schema.MigrateOption
+	}
+)
+
+// WithOptions forwards options to client creation.
+func WithOptions(opts ...entv1.Option) Option {
+	return func(o *options) {
+		o.opts = append(o.opts, opts...)
+	}
+}
+
+// WithMigrateOptions forwards options to auto migration.
+func WithMigrateOptions(opts ...schema.MigrateOption) Option {
+	return func(o *options) {
+		o.migrateOpts = append(o.migrateOpts, opts...)
+	}
+}
+
+func newOptions(opts []Option) *options {
+	o := &options{}
+	for _, opt := range opts {
+		opt(o)
+	}
+	return o
 }
 
 // Open calls entv1.Open and auto-run migration.
-func Open(t TestingT, driverName, dataSourceName string, options ...schema.MigrateOption) *entv1.Client {
-	c, err := entv1.Open(driverName, dataSourceName)
+func Open(t TestingT, driverName, dataSourceName string, opts ...Option) *entv1.Client {
+	o := newOptions(opts)
+	c, err := entv1.Open(driverName, dataSourceName, o.opts...)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-	if err := c.Schema.Create(context.Background(), options...); err != nil {
+	if err := c.Schema.Create(context.Background(), o.migrateOpts...); err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	return c
+}
+
+// NewClient calls entv1.NewClient and auto-run migration.
+func NewClient(t TestingT, opts ...Option) *entv1.Client {
+	o := newOptions(opts)
+	c := entv1.NewClient(o.opts...)
+	if err := c.Schema.Create(context.Background(), o.migrateOpts...); err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
--- entc/integration/migrate/entv2/enttest/enttest.go
+++ entc/integration/migrate/entv2/enttest/enttest.go
@@ -9,27 +9,72 @@ package enttest
 import (
 	"context"
 
-	"github.com/facebookincubator/ent/entc/integration/migrate/entv2"
-	_ "github.com/facebookincubator/ent/entc/integration/migrate/entv2/runtime"
-
 	"github.com/facebookincubator/ent/dialect/sql/schema"
+	"github.com/facebookincubator/ent/entc/integration/migrate/entv2"
+
+	// required by schema hooks.
+	_ "github.com/facebookincubator/ent/entc/integration/migrate/entv2/runtime"
 )
 
-// TestingT is the interface that is shared between
-// testing.T and testing.B and used by enttest.
-type TestingT interface {
-	FailNow()
-	Error(...interface{})
+type (
+	// TestingT is the interface that is shared between
+	// testing.T and testing.B and used by enttest.
+	TestingT interface {
+		FailNow()
+		Error(...interface{})
+	}
+
+	// Option configures client creation.
+	Option func(*options)
+
+	options struct {
+		opts        []entv2.Option
+		migrateOpts []schema.MigrateOption
+	}
+)
+
+// WithOptions forwards options to client creation.
+func WithOptions(opts ...entv2.Option) Option {
+	return func(o *options) {
+		o.opts = append(o.opts, opts...)
+	}
+}
+
+// WithMigrateOptions forwards options to auto migration.
+func WithMigrateOptions(opts ...schema.MigrateOption) Option {
+	return func(o *options) {
+		o.migrateOpts = append(o.migrateOpts, opts...)
+	}
+}
+
+func newOptions(opts []Option) *options {
+	o := &options{}
+	for _, opt := range opts {
+		opt(o)
+	}
+	return o
 }
 
 // Open calls entv2.Open and auto-run migration.
-func Open(t TestingT, driverName, dataSourceName string, options ...schema.MigrateOption) *entv2.Client {
-	c, err := entv2.Open(driverName, dataSourceName)
+func Open(t TestingT, driverName, dataSourceName string, opts ...Option) *entv2.Client {
+	o := newOptions(opts)
+	c, err := entv2.Open(driverName, dataSourceName, o.opts...)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-	if err := c.Schema.Create(context.Background(), options...); err != nil {
+	if err := c.Schema.Create(context.Background(), o.migrateOpts...); err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	return c
+}
+
+// NewClient calls entv2.NewClient and auto-run migration.
+func NewClient(t TestingT, opts ...Option) *entv2.Client {
+	o := newOptions(opts)
+	c := entv2.NewClient(o.opts...)
+	if err := c.Schema.Create(context.Background(), o.migrateOpts...); err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
