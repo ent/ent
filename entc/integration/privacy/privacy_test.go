d7bfda98525201f51431fc4bdbaee9334ffec1f8
Ariel Mashraki <ariel@mashraki.co.il>
2020-05-04 20:10:00 +0300

    entc/integ: add missing license header

--- entc/integration/privacy/privacy_test.go
+++ entc/integration/privacy/privacy_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
 package privacy
 
 import (
