66255ea86e4fa4841e99311f62f7f5ca6080f430
Alex Snast <alexsn@fb.com>
2020-05-05 15:53:15 +0300

    entc/gen/privacy: adding decision context support (#462)
    Decision context holds a privacy decision (i.e. allow / deny) and if
    exists will be returned before any rule is evaluated.
    
    Signed-off-by: Alex Snast <alexsn@fb.com>

--- entc/integration/privacy/privacy_test.go
+++ entc/integration/privacy/privacy_test.go
@@ -9,11 +9,10 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/facebookincubator/ent/entc/integration/privacy/ent"
+	"github.com/facebookincubator/ent/entc/integration/privacy/ent/enttest"
 	"github.com/facebookincubator/ent/entc/integration/privacy/ent/galaxy"
 	"github.com/facebookincubator/ent/entc/integration/privacy/ent/planet"
 	"github.com/facebookincubator/ent/entc/integration/privacy/ent/privacy"
-	_ "github.com/facebookincubator/ent/entc/integration/privacy/ent/runtime"
 	"github.com/facebookincubator/ent/entc/integration/privacy/rule"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -21,29 +20,46 @@ import (
 )
 
 func TestPrivacyRules(t *testing.T) {
-	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
-	require.NoError(t, err)
+	client := enttest.Open(t, "sqlite3",
+		"file:ent?mode=memory&cache=shared&_fk=1",
+	)
 	defer client.Close()
-	ctx := context.Background()
-	err = client.Schema.Create(ctx)
-	require.NoError(t, err)
 	logf := rule.SetMutationLogFunc(t.Logf)
 	defer rule.SetMutationLogFunc(logf)
 
-	earth, err := client.Planet.Create().SetName("Earth").SetAge(4_540_000_000).Save(ctx)
+	ctx := context.Background()
+	earth, err := client.Planet.Create().
+		SetName("Earth").
+		SetAge(4_540_000_000).
+		Save(ctx)
 	require.NoError(t, err)
-	mars := client.Planet.Create().SetName("Mars").SaveX(ctx)
-	err = earth.Update().AddNeighbors(mars).Exec(ctx)
+	mars := client.Planet.Create().
+		SetName("Mars").
+		SaveX(ctx)
+	err = earth.Update().
+		AddNeighbors(mars).
+		Exec(ctx)
 	require.NoError(t, err)
 
 	logf = rule.SetMutationLogFunc(func(string, ...interface{}) {
 		require.FailNow(t, "hook called on privacy deny")
 	})
-	err = client.Planet.Update().Where(planet.ID(earth.ID)).SetAge(4_600_000_000).Exec(ctx)
+	err = client.Planet.Update().
+		Where(planet.ID(earth.ID)).
+		SetAge(4_600_000_000).
+		Exec(ctx)
 	require.True(t, errors.Is(err, privacy.Deny))
-	err = earth.Update().AddNeighbors(earth).Exec(ctx)
+	err = earth.Update().
+		AddNeighbors(earth).
+		Exec(ctx)
 	require.True(t, errors.Is(err, privacy.Deny))
 	rule.SetMutationLogFunc(logf)
+
+	err = client.Planet.Update().
+		Where(planet.ID(earth.ID)).
+		SetAge(4_600_000_000).
+		Exec(privacy.DecisionContext(ctx, privacy.Allow))
+	require.NoError(t, err)
 
 	count := client.Planet.Query().CountX(ctx)
 	require.Equal(t, 1, count)
@@ -51,8 +67,15 @@ func TestPrivacyRules(t *testing.T) {
 	count = client.Planet.Query().CountX(ctx)
 	require.Equal(t, 2, count)
 
-	client.Galaxy.Create().SetName("Milky Way").SetType(galaxy.TypeBarredSpiral).AddPlanets(earth, mars).SaveX(ctx)
-	client.Galaxy.Create().SetName("IC 3583").SetType(galaxy.TypeIrregular).SaveX(ctx)
+	client.Galaxy.Create().
+		SetName("Milky Way").
+		SetType(galaxy.TypeBarredSpiral).
+		AddPlanets(earth, mars).
+		SaveX(ctx)
+	client.Galaxy.Create().
+		SetName("IC 3583").
+		SetType(galaxy.TypeIrregular).
+		SaveX(ctx)
 	count = client.Galaxy.Query().CountX(ctx)
 	require.Equal(t, 1, count)
 }
