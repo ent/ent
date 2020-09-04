68f6ca7bdbe7718c914af4abd37137f86fed073b
Nicholas Xu <shengfeng.xu@hotmail.com>
2020-09-04 16:31:20 +0800

    fix update problem

--- dialect/sql/sqlgraph/graph.go
+++ dialect/sql/sqlgraph/graph.go
@@ -652,6 +652,9 @@ func (u *updater) nodes(ctx context.Context, tx dialect.ExecQuerier) (int, error
 	)
 	selector := u.builder.Select(u.Node.ID.Column).
 		From(u.builder.Table(u.Node.Table))
+
+	archivedWherePredicate := selector.Clone().P()
+
 	if pred := u.Predicate; pred != nil {
 		pred(selector)
 	}
@@ -670,7 +673,7 @@ func (u *updater) nodes(ctx context.Context, tx dialect.ExecQuerier) (int, error
 	if len(ids) == 0 {
 		return 0, nil
 	}
-	update := u.builder.Update(u.Node.Table).Where(selector.Clone().P())
+	update := u.builder.Update(u.Node.Table).Where(archivedWherePredicate)
 	if err := u.setTableColumns(update, addEdges, clearEdges); err != nil {
 		return 0, err
 	}
