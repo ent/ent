8de9630f8a59366dfe703fd48cddf84ce5397f4a
Eric Mason <thecryptoraven@gmail.com>
2020-10-15 18:45:45 +0200

    [test] Adds tests for init --entity argument

--- cmd/entc/entc_test.go
+++ cmd/entc/entc_test.go
@@ -15,6 +15,8 @@ import (
 
 func TestCmd(t *testing.T) {
 	defer os.RemoveAll("ent")
+	defer os.RemoveAll("entities")
+	defer os.RemoveAll("schemas")
 	cmd := exec.Command("go", "run", "github.com/facebook/ent/cmd/entc", "init", "User")
 	stderr := bytes.NewBuffer(nil)
 	cmd.Stderr = stderr
@@ -24,6 +26,41 @@ func TestCmd(t *testing.T) {
 	require.NoError(t, err)
 	_, err = os.Stat("ent/schema/user.go")
 	require.NoError(t, err)
+
+	cmd = exec.Command(
+		"go",
+		"run",
+		"github.com/facebook/ent/cmd/entc",
+		"init",
+		"--target",
+		"schemas",
+		"--entity",
+		"entities",
+		"User",
+	)
+	stderr = bytes.NewBuffer(nil)
+	cmd.Stderr = stderr
+	require.NoError(t, cmd.Run(), stderr.String())
+
+	_, err = os.Stat("entities/generate.go")
+	require.NoError(t, err)
+	_, err = os.Stat("schemas/user.go")
+	require.NoError(t, err)
+
+	cmd = exec.Command(
+		"go",
+		"run",
+		"github.com/facebook/ent/cmd/entc",
+		"init",
+		"--target",
+		"samedir",
+		"--entity",
+		"samedir",
+		"User",
+	)
+	stderr = bytes.NewBuffer(nil)
+	cmd.Stderr = stderr
+	require.Error(t, cmd.Run(), "schemas and entities can't be generated in the same path. see: entc help init")
 
 	cmd = exec.Command("go", "run", "github.com/facebook/ent/cmd/entc", "generate", "./ent/schema")
 	stderr = bytes.NewBuffer(nil)
