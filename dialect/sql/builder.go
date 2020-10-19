d6fd283f68072691e18bcb51173cd4386deb1e2f
maxilozoz <2802146520@qq.com>
2020-10-19 19:47:17 +0800

    dialect/sql: add support for selector in the notIn predicate (#861)
    * `notIn` support Selector like `in`
    
    * `notIn` args check

--- dialect/sql/builder.go
+++ dialect/sql/builder.go
@@ -1054,10 +1054,17 @@ func NotIn(col string, args ...interface{}) *Predicate {
 
 // NotIn appends the `Not IN` predicate.
 func (p *Predicate) NotIn(col string, args ...interface{}) *Predicate {
+	if len(args) == 0 {
+		return p
+	}
 	return p.Append(func(b *Builder) {
 		b.Ident(col).WriteOp(OpNotIn)
 		b.Nested(func(b *Builder) {
-			b.Args(args...)
+			if s, ok := args[0].(*Selector); ok {
+				b.Join(s)
+			} else {
+				b.Args(args...)
+			}
 		})
 	})
 }
