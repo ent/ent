2e9ec60941698469577b6f6e09d5ba11473f182e
Ariel Mashraki <7413593+a8m@users.noreply.github.com>
2020-10-28 18:11:33 +0200

    Revert "entc/gen: invoke chain hook in order to registration (#898)"
    This reverts commit cdf2107cf8cbd7a43b614f26ec9ab868106cf399.

--- entc/integration/migrate/entv1/hook/hook.go
+++ entc/integration/migrate/entv1/hook/hook.go
@@ -198,7 +198,7 @@ func NewChain(hooks ...entv1.Hook) Chain {
 // Hook chains the list of hooks and returns the final hook.
 func (c Chain) Hook() entv1.Hook {
 	return func(mutator entv1.Mutator) entv1.Mutator {
-		for i := range c.hooks {
+		for i := len(c.hooks) - 1; i >= 0; i-- {
 			mutator = c.hooks[i](mutator)
 		}
 		return mutator
