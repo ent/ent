5f50fa65c0d5417475917e6b62d8dcc7fcdde70b
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2021-03-13 10:12:31 +1000

    Fix postgres tests

--- entc/integration/upsert/upsert_test.go
+++ entc/integration/upsert/upsert_test.go
@@ -54,7 +54,7 @@ func Test_Postgres_UserUpsertBulk(t *testing.T) {
 	client := enttest.Open(t, dialect.Postgres, dsn+" dbname=upsert_test", enttest.WithMigrateOptions(migrate.WithGlobalUniqueID(true)))
 	defer client.Close()
 
-	runBulkInsertTest(ctx, t, client)
+	t.Run(bulkUpsert(ctx, client))
 }
 
 func Test_MySQL_UserUpsertBulk(t *testing.T) {
@@ -64,18 +64,18 @@ func Test_MySQL_UserUpsertBulk(t *testing.T) {
 			root, err := entsql.Open("mysql", fmt.Sprintf("root:pass@tcp(localhost:%d)/", port))
 			require.NoError(t, err)
 			defer root.Close()
-			err = root.Exec(ctx, "CREATE DATABASE IF NOT EXISTS config", []interface{}{}, new(sql.Result))
+			err = root.Exec(ctx, "CREATE DATABASE IF NOT EXISTS upsert_test", []interface{}{}, new(sql.Result))
 			require.NoError(t, err, "creating database")
-			defer root.Exec(ctx, "DROP DATABASE IF EXISTS config", []interface{}{}, new(sql.Result))
+			defer root.Exec(ctx, "DROP DATABASE IF EXISTS upsert_test", []interface{}{}, new(sql.Result))
 
-			drv, err := entsql.Open("mysql", fmt.Sprintf("root:pass@tcp(localhost:%d)/config?parseTime=True", port))
+			drv, err := entsql.Open("mysql", fmt.Sprintf("root:pass@tcp(localhost:%d)/upsert_test?parseTime=True", port))
 			require.NoError(t, err, "connecting to migrate database")
 
 			client := ent.NewClient(ent.Driver(drv))
 			// Run schema creation.
 			require.NoError(t, client.Schema.Create(ctx))
 
-			runBulkInsertTest(ctx, t, client)
+			t.Run(bulkUpsert(ctx, client))
 		})
 	}
 }
@@ -85,33 +85,35 @@ func Test_SQLLite_UserUpsertBulk(t *testing.T) {
 	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1", enttest.WithMigrateOptions(migrate.WithGlobalUniqueID(true)))
 	defer client.Close()
 
-	runBulkInsertTest(ctx, t, client)
+	t.Run(bulkUpsert(ctx, client))
 }
 
-func runBulkInsertTest(ctx context.Context, t *testing.T, client *ent.Client) {
-	_, err := client.Debug().User.Create().
-		SetEmail("roger-test@entgo.io").
-		OnConflict("email").
-		Save(ctx)
-	require.NoError(t, err)
+func bulkUpsert(ctx context.Context, client *ent.Client) (string, func(*testing.T)) {
+	return "Bulk Upsert", func(t *testing.T) {
+		_, err := client.Debug().User.Create().
+			SetEmail("roger-test@entgo.io").
+			OnConflict("email").
+			Save(ctx)
+		require.NoError(t, err)
 
-	uc1 := client.User.Create().
-		SetEmail("alex-test@entgo.io").
-		SetUpdateCount(1).
-		OnConflict("email")
-	uc2 := client.User.Create().
-		SetEmail("roger-test@entgo.io").
-		SetUpdateCount(2).
-		OnConflict("email")
-	uc3 := client.User.Create().
-		SetEmail("alex-test@entgo.io").
-		SetUpdateCount(2).
-		OnConflict("email")
-	users, err := client.Debug().User.CreateBulk(uc1, uc2, uc3).Save(ctx)
+		_, err = client.Debug().User.Create().
+			SetEmail("alex-test@entgo.io").
+			OnConflict("email").
+			Save(ctx)
+		require.NoError(t, err)
 
-	require.NoError(t, err)
-	require.NotNil(t, users, "User record was inserted")
+		users, err := client.Debug().User.CreateBulk(
+			client.User.Create().SetEmail("roger-test@entgo.io").SetUpdateCount(2).OnConflict("email"),
+			client.User.Create().SetEmail("alex-test@entgo.io").SetUpdateCount(2).OnConflict("email"),
+		).Save(ctx)
 
-	user := client.User.Query().Where(user.Email(("alex-test@entgo.io"))).OnlyX(ctx)
-	require.Equal(t, 2, user.UpdateCount, "User was updated correctly on conflict")
+		require.NoError(t, err)
+		require.NotNil(t, users, "User record was inserted")
+
+		user1 := client.User.Query().Where(user.Email(("alex-test@entgo.io"))).OnlyX(ctx)
+		require.Equal(t, 2, user1.UpdateCount, "User was updated correctly on conflict")
+
+		user2 := client.User.Query().Where(user.Email(("roger-test@entgo.io"))).OnlyX(ctx)
+		require.Equal(t, 2, user2.UpdateCount, "User was updated correctly on conflict")
+	}
 }
