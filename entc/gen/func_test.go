449c07b39b94822172e4cb8c6aad256252bdb1f3
Ricky Pai <rickyp999+github@gmail.com>
2021-03-17 15:53:31 -0700

    existing tests for snake

--- entc/gen/func_test.go
+++ entc/gen/func_test.go
@@ -4,7 +4,9 @@
 
 package gen
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_receiver(t *testing.T) {
 	tests := []struct {
@@ -48,6 +50,36 @@ func Test_receiver(t *testing.T) {
 		t.Run(tt.arg, func(t *testing.T) {
 			if got := receiver(tt.arg); got != tt.want {
 				t.Errorf("receiver() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_snake(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		arg  string
+		want string
+	}{
+		{
+			"Username",
+			"username",
+		},
+		{
+			"FullName",
+			"full_name",
+		},
+		{
+			"HTTPCode",
+			"http_code",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.arg, func(t *testing.T) {
+			if got := snake(tt.arg); got != tt.want {
+				t.Errorf("snake() = %v, want %v", got, tt.want)
 			}
 		})
 	}
