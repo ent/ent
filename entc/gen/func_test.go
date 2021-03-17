c421327d68a4a3bfba7cd6049236121d1ba91c1d
Ricky Pai <rickyp999+github@gmail.com>
2021-03-17 15:40:28 -0700

    add receiver test

---
+++ entc/gen/func_test.go
@@ -0,0 +1,30 @@
+// Copyright 2019-present Facebook Inc. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package gen
+
+import "testing"
+
+func Test_receiver(t *testing.T) {
+	tests := []struct {
+		arg  string
+		want string
+	}{
+		{
+			"UserQuery",
+			"uq",
+		},
+		{
+			"UserSelect",
+			"us",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.arg, func(t *testing.T) {
+			if got := receiver(tt.arg); got != tt.want {
+				t.Errorf("receiver() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
