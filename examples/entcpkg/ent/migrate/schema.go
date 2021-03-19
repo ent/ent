aee6785cbacdd659a6494be07f44659f496c6312
Ariel Mashraki <7413593+a8m@users.noreply.github.com>
2021-03-19 12:29:51 +0200

    examples/entcpkg: add codegen-hook example (#1356)

--- examples/entcpkg/ent/migrate/schema.go
+++ examples/entcpkg/ent/migrate/schema.go
@@ -15,6 +15,8 @@ var (
 	// UsersColumns holds the columns for the "users" table.
 	UsersColumns = []*schema.Column{
 		{Name: "id", Type: field.TypeInt, Increment: true},
+		{Name: "name", Type: field.TypeString, Nullable: true},
+		{Name: "age", Type: field.TypeInt, Nullable: true},
 	}
 	// UsersTable holds the schema information for the "users" table.
 	UsersTable = &schema.Table{
