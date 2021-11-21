2aa39e17843fb886389c264b3f1ead197dacca79
hedwigz <amit@ariga.io>
2021-11-21 16:00:37 +0200

    ent/doc: load github buttons from cdnjs

--- doc/website/docusaurus.config.js
+++ doc/website/docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
   "organizationName": "ent",
   "projectName": "ent",
   "scripts": [
-    "https://buttons.github.io/buttons.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/github-buttons/2.20.0/buttons.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
     "/js/code-block-buttons.js",
     "/js/custom.js"
