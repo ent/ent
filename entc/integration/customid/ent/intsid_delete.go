f438e2ea6fa78c7a6bb0dfd6edb11972427f992c
Reiji Tokuda <tokuda@rtez.net>
2022-06-24 07:17:41 +0800

    Update generated files after merge from master

--- entc/integration/customid/ent/intsid_delete.go
+++ entc/integration/customid/ent/intsid_delete.go
@@ -88,7 +88,11 @@ func (isd *IntSIDDelete) sqlExec(ctx context.Context) (int, error) {
 			}
 		}
 	}
-	return sqlgraph.DeleteNodes(ctx, isd.driver, _spec)
+	affected, err := sqlgraph.DeleteNodes(ctx, isd.driver, _spec)
+	if err != nil && sqlgraph.IsConstraintError(err) {
+		err = &ConstraintError{msg: err.Error(), wrap: err}
+	}
+	return affected, err
 }
 
 // IntSIDDeleteOne is the builder for deleting a single IntSID entity.
