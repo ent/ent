aca45a5716e339923254224852c8cf03e3a1bb02
Sinh Ngo <kaka.ngo@gmail.com>
2022-10-24 16:59:14 +0700

    BE-2288: Scaffold one-to-one example

---
+++ examples/dynamodb/o2o2types/ent/schema/card.go
@@ -0,0 +1,37 @@
+// Copyright 2019-present Facebook Inc. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Card holds the schema definition for the Card entity.
+type Card struct {
+	ent.Schema
+}
+
+// Fields of the Card.
+func (Card) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("expired"),
+		field.String("number"),
+	}
+}
+
+// Edges of the Card.
+func (Card) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("owner", User.Type).
+			Ref("card").
+			Unique().
+			// We add the "Required" method to the builder
+			// to make this edge required on entity creation.
+			// i.e. Card cannot be created without its owner.
+			Required(),
+	}
+}
---
+++ examples/dynamodb/o2o2types/ent/schema/user.go
@@ -0,0 +1,32 @@
+// Copyright 2019-present Facebook Inc. All rights reserved.
+// This source code is licensed under the Apache 2.0 license found
+// in the LICENSE file in the root directory of this source tree.
+
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("age"),
+		field.String("name"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("card", Card.Type).
+			Unique(),
+	}
+}
