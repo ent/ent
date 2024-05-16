2f5b8cee56099975c7616d5f2ff669e8ea126f08
Patt-tom McDonnell <contact@mcdonnell.pt>
2024-05-16 15:39:24 +0100

    Add error handling for nil builders when calling Exec for Upsert

--- entc/integration/edgeschema/ent/process_create.go
+++ entc/integration/edgeschema/ent/process_create.go
@@ -444,6 +444,9 @@ func (u *ProcessUpsertBulk) Exec(ctx context.Context) error {
 		return u.create.err
 	}
 	for i, b := range u.create.builders {
+		if b == nil {
+			return fmt.Errorf("ent: missing builder at index %d, unexpected nil builder passed to CreateBulk", i)
+		}
 		if len(b.conflict) != 0 {
 			return fmt.Errorf("ent: OnConflict was set for builder %d. Set it on the ProcessCreateBulk instead", i)
 		}
